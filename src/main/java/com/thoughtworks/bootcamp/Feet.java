ef4e53749555faa2a24386330db5bb014b66e2d9
snehamanojraopalekar <nehapalekar026@gmail.com>
2019-09-27 12:01:08 +0530

    [Shyam|Sneha] Fix. Replace Comapare with equal method

--- src/main/java/com/thoughtworks/bootcamp/Feet.java
+++ src/main/java/com/thoughtworks/bootcamp/Feet.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.bootcamp;
 
-import java.util.Objects;
 
 public class Feet {
     private final double value;
@@ -11,14 +10,8 @@ public class Feet {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
         Feet feet = (Feet) o;
         return Double.compare(feet.value, value) == 0;
     }
 
-
-    public boolean compare(Feet feet) {
-        return equals(feet);
-    }
 }
--- src/test/java/com/thoughtworks/bootcamp/FeetTest.java
+++ src/test/java/com/thoughtworks/bootcamp/FeetTest.java
@@ -1,30 +1,36 @@
 package com.thoughtworks.bootcamp;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class FeetTest {
+
     @Test
-    void givenZeroFeetAndZeoFeet_WhenEquals_ThenShouldBeEqual(){
+    void givenZeroFeetAndZeoFeet_WhenCompare_ThenShouldBeEqual() {
+
         Feet zeroFeetOne = new Feet(0.0);
-        Feet zeroFeetTwo=new Feet(0.0);
-        assertTrue(zeroFeetOne.equals(zeroFeetTwo));
+        Feet zeroFeetTwo = new Feet(0.0);
+
+        assertEquals(zeroFeetOne, zeroFeetTwo);
     }
 
     @Test
-    void givenOneFeetAndOneFeet_WhenCompare_ThenShouldBeEqual(){
+    void givenOneFeetAndOneFeet_WhenCompare_ThenShouldBeEqual() {
         Feet oneFeetOne = new Feet(1.0);
-        Feet oneFeetTwo=new Feet(1.0);
-        assertTrue(oneFeetOne.compare(oneFeetTwo));
+        Feet oneFeetTwo = new Feet(1.0);
+
+        assertEquals(oneFeetOne, oneFeetTwo);
     }
 
     @Test
-    void givenTwoFeetAndTwoFeet_WhenCompare_ThenShouldNotBeEqual(){
+    void givenTwoFeetAndTwoFeet_WhenCompare_ThenShouldNotBeEqual() {
         Feet oneFeetOne = new Feet(1.0);
-        Feet twoFeetTwo=new Feet(2.0);
-        assertFalse(oneFeetOne.compare(twoFeetTwo));
+        Feet twoFeetTwo = new Feet(2.0);
+        assertNotEquals(oneFeetOne, twoFeetTwo);
     }
+
 
 }
